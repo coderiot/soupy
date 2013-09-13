52c0294c5088227250d671570d271cfaaefc4ff5
peterrr <peterrr@github>
2013-09-13 15:05:23 +0200

    new version of soupy package:0.2.0  * refactoring soupy.py in folder to soupy  * split soupy.SoupAccount into modules blog, user and request  * switch from mechanize to python-requests

--- setup.py
+++ setup.py
@@ -3,14 +3,15 @@
 
 from distutils.core import setup
 
+
 setup(name='soupy',
-    version='0.1.0',
+    version='0.2.0',
     license='GPLv2',
     description='description',
     author='peterrr',
     url='https://github.com/peterrr/soupy/',
     install_requires=['lxml >= 3.0',
                       'cssselect >= 0.8',
-                      'mechanize'],
-    py_modules=['soupy']
+                      'requests >= 1.2.3'],
+    packages=['soupy']
 )
